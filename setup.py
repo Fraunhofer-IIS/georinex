1992b032d5df47cc8c483a8c6ee652eafe6c8151
Xabier Zubizarreta <xabier.zubizarreta@iis.fraunhofer.de>
2018-12-12 15:18:35 +0100

    Extend setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python
-from setuptools import setup
-setup()
+from setuptools import setup, find_packages
+setup(name='georinex',
+      version='1.0.0',
+      description='georinex (fork from Fraunhofer IIS)',
+      url='https://github.com/Fraunhofer-IIS/georinex',
+      keyword=['georinex'],
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False)
+
